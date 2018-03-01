6f854ccdf5e70a85aa81ad0bc45c4c619807e04d
joeyyellow2005 <coderjoe05@gmail.com>
2018-03-01 07:17:30 -0500

    Merge remote-tracking branch 'origin/BTCui-finish-and-polish' into BTCui-finish-and-polish

--- CoinPrice/AppDelegate.swift
+++ CoinPrice/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import IQKeyboardManagerSwift
+import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         IQKeyboardManager.sharedManager().enable = true
+        GADMobileAds.configure(withApplicationID: "ca-app-pub-4026820850636406~1751726648")
         return true
     }
 
