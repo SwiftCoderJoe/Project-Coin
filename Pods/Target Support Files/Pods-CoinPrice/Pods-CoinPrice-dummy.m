6cb0e7e3e814bbd3b0529806d0f604ee41c52b26
JoeyCMacBook <coderjoe05@icloud.com>
2018-03-01 08:56:20 -0500

    Delete Revert Phase 1

--- Pods/Target Support Files/Pods-CoinPrice/Pods-CoinPrice-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CoinPrice : NSObject
-@end
-@implementation PodsDummy_Pods_CoinPrice
-@end
--- Pods/Target Support Files/Pods-CoinPriceTests/Pods-CoinPriceTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CoinPriceTests : NSObject
-@end
-@implementation PodsDummy_Pods_CoinPriceTests
-@end
