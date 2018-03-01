363dfe231272c5372c5e7d13933076bece9728f1
JoeyCMacBook <coderjoe05@icloud.com>
2018-03-01 08:56:36 -0500

    Re-add revert phase 2

---
+++ CoinPrice/DateFormatter.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  CoinPrice
+//
+//  Created by Kids on 1/23/18.
+//  Copyright © 2018 BytleBit. All rights reserved.
+//
+
+
+import Foundation
+import Charts
+let boi:Double = 50
+
+public class DateValueFormatter: NSObject, IAxisValueFormatter {
+    private let dateFormatter = DateFormatter()
+    
+    override init() {
+        super.init()
+        dateFormatter.dateFormat = "MM-dd"
+    }
+    
+    public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        return dateFormatter.string(from: Calendar.current.date(byAdding: .day, value: Int(-(boi - value)), to: Date())!)
+    }
+}
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'CoinPrice' do
+# Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  
+  pod 'NVActivityIndicatorView'
+  
+  pod 'Alamofire', '~> 4.5'
+
+  pod 'Charts'
+  
+  pod 'IQKeyboardManagerSwift'
+  
+  target 'CoinPriceTests' do
+    inherit! :search_paths
+  end
+
+end
